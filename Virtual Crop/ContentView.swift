1ef922540e65ddfc99302b5cbf3902ed699aeabd
Djenifer R. P. <djeniferrenata@yahoo.com.br>
2024-01-17 11:16:14 -0300

    Show image and add simple gestures

--- Virtual Crop/ContentView.swift
+++ Virtual Crop/ContentView.swift
@@ -1,14 +1,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var uiimage = UIImage(named: "image-900-500")!
+    @State private var scale: CGFloat = 1.0
+    @State private var offset = CGSize.zero
+
+    var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                self.offset = value.translation
+            }
+    }
+
+    var scaleGesture: some Gesture {
+        MagnificationGesture()
+            .onChanged { value in
+                self.scale = value.magnitude
+            }
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image(uiImage: uiimage)
+                .scaleEffect(scale)
+                .offset(offset)
         }
-        .padding()
+        .gesture(dragGesture)
+        .simultaneousGesture(scaleGesture)
     }
 }
 
