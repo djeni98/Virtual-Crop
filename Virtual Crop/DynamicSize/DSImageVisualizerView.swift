f17e203cfae3350b47853a8f2ee081b49a482d7e
Djenifer R. P. <djeniferrenata@yahoo.com.br>
2024-01-19 10:35:21 -0300

    Rescale image with cropsize in visualizer

--- Virtual Crop/DynamicSize/DSImageVisualizerView.swift
+++ Virtual Crop/DynamicSize/DSImageVisualizerView.swift
@@ -1,16 +1,31 @@
 import SwiftUI
 
+fileprivate let values: [CGFloat] = [
+    100, 150, 200, 250, 300
+]
+
 struct DynamicSizeImageVisualizerView: View {
     let imageInfo: DynamicSizeImageInfo
-    private var size: CGSize { DynamicSizeHomeView.cropSize }
+    private let value: CGFloat = values.randomElement()!
+    private var size: CGSize { CGSize(width: value, height: value) }
 
     var body: some View {
         Image(uiImage: imageInfo.uiimage)
             .scaleEffect(imageInfo.scale ?? 1)
             .offset(imageInfo.offset ?? .zero)
+            .scaleEffect(getReScaleFactor() ?? 1)
             .frame(width: size.width, height: size.height)
             .clipShape(Circle())
             .contentShape(Circle())
+    }
+
+    func getReScaleFactor() -> CGFloat? {
+        guard let cropperSize = imageInfo.cropperSize else { return nil }
+
+        let scaleHeight = size.height / cropperSize.height
+        let scaleWidth = size.width / cropperSize.width
+
+        return (scaleHeight + scaleWidth) / 2
     }
 }
 
